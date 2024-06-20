2fb23524ba07ea480018b66f9de046901446dd33
mkamepal <kamepallimaheshbabu6007@gmail.com>
2024-06-20 13:13:08 +0200

    Changes in data_collection to load .env variables

--- data_collection.py
+++ data_collection.py
@@ -9,10 +9,10 @@ import os
 app = Flask(__name__)
 
 session = boto3.Session(
-    aws_access_key_id='',
-    aws_secret_access_key='',
-    aws_session_token='',
-    region_name='us-east-1'
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+    aws_session_token=os.getenv('AWS_SESSION_TOKEN'),
+    region_name=os.getenv('AWS_REGION')
     )
 
 # Initialize S3 client

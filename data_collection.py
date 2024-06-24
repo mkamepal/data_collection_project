c92d5ffa587c7ee7e883f9d4a581408459180fed
mkamepal <kamepallimaheshbabu6007@gmail.com>
2024-06-24 13:14:51 +0200

    add code

--- data_collection.py
+++ data_collection.py
@@ -8,6 +8,8 @@ import os
 
 app = Flask(__name__)
 
+load_dotenv()
+
 session = boto3.Session(
     aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
     aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
@@ -17,6 +19,8 @@ session = boto3.Session(
 
 # Initialize S3 client
 s3_client = session.client('s3')
+#firehouse_client = session.client('firehose')
+#delivery_stream_name = 'your-delivery-stream-name'
 
 # S3 bucket name and file key
 bucket_name = 'prompts-dataset'
@@ -24,21 +28,32 @@ file_key = 'data-collection.xlsx'
 
 @app.route('/put', methods=['POST'])
 def put_data():
-    print("hey i am here in put function")
     data = request.json
-    print(data)
     new_data = pd.DataFrame([data])
 
-    # Save the new data to an Excel file
+    # load the existing Excel file from S3
+    try:
+        response = s3_client.get_object(Bucket=bucket_name, Key=file_key)
+        contents = response['Body'].read()
+        existing_data = pd.read_excel(io.BytesIO(contents))
+    except s3_client.exceptions.NoSuchKey:
+        # If the file does not exist, create an empty DataFrame
+        existing_data = pd.DataFrame()
+
+    # Append the new data to the existing data
+    updated_data = pd.concat([existing_data, new_data], ignore_index=True)
+
+    # Save the updated data to an Excel file
     with io.BytesIO() as output:
         with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
-            new_data.to_excel(writer, index=False)
+            updated_data.to_excel(writer, index=False)
         data_to_upload = output.getvalue()
 
-    # Upload the new Excel file to S3
+    # Upload the updated Excel file to S3
     s3_client.put_object(Bucket=bucket_name, Key=file_key, Body=data_to_upload)
 
     return jsonify({'message': 'Data added successfully'})
+
 
 @app.route('/get', methods=['GET'])
 def get_data():
